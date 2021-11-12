2ff2102345cc6fa6ddb15de6e175ddef9d45adc1
jamesray23114 <jamesray23114@gmail.com>
2021-11-12 12:32:30 -0800

    Aozora-OS-1.0.4 -> finished ability to print registers, added ability to print hex

--- src/common/x16/print16.asm
+++ src/common/x16/print16.asm
@@ -6,26 +6,28 @@ global printdec, printcstr, printchar, clearscreen, printregs
 section .boot2
 
 regdata:
-    .ax: dw 1
-    .bx: dw 1
-    .cx: dw 1
-    .dx: dw 1
-    .si: dw 1
-    .di: dw 1
-    .bp: dw 1
-    .sp: dw 1
+    .ax: dw 0
+    .bx: dw 0
+    .cx: dw 0
+    .dx: dw 0
+    .bp: dw 0
+    .sp: dw 0
 .:
 
-logal: db "AL = ...    0b........          0x.."  , 0 
-logah: db "AH = ...    0b........          0x.."  , 0 
-logbl: db "BL = ...    0b........          0x.."  , 0 
-logbh: db "BH = ...    0b........          0x.."  , 0 
-logcl: db "CL = ...    0b........          0x.."  , 0 
-logch: db "CH = ...    0b........          0x.."  , 0 
-logdl: db "DL = ...    0b........          0x.."  , 0 
-logdh: db "DH = ...    0b........          0x.."  , 0 
-logsp: db "SP = .....  0b................  0x....", 0
-logbp: db "BP = .....  0b................  0x....", 0
+log: 
+    .al: db "AL = ...    0b........          0x.."  , 0 
+    .ah: db "AH = ...    0b........          0x.."  , 0 
+         db "BL = ...    0b........          0x.."  , 0 
+         db "BH = ...    0b........          0x.."  , 0 
+         db "CL = ...    0b........          0x.."  , 0 
+         db "CH = ...    0b........          0x.."  , 0 
+         db "DL = ...    0b........          0x.."  , 0 
+         db "DH = ...    0b........          0x.."  , 0 
+         db "SP = .....  0b................  0x....", 0
+         db "BP = .....  0b................  0x....", 0
+    .size: db ($ - log) / (.ah - .al) + 2
+.:
+
 
 printregs:
 
@@ -34,83 +36,93 @@ printregs:
     mov [regdata.cx], cx
     mov [regdata.dx], dx
 
-    mov [regdata.di], di
     mov [regdata.bp], bp
     mov [regdata.sp], sp
 
-    __movcur 0, 1
-    __printcstr logal
-    __movcur 0, 2
-    __printcstr logah
+    mov cx, 2
+    mov si, log.al
 
-    __movcur 0, 3
-    __printcstr logbl
-    __movcur 0, 4
-    __printcstr logbh
+    .loopstr:
+        __movcur 0, cl
+        __printcstr si
 
-    __movcur 0, 5
-    __printcstr logcl
-    __movcur 0, 6
-    __printcstr logch
+        inc cx
 
-    __movcur 0, 7
-    __printcstr logdl
-    __movcur 0, 8
-    __printcstr logdh
-
-    __movcur 0, 10
-    __printcstr logsp
-    __movcur 0, 11
-    __printcstr logbp
+        cmp cl, [log.size]
+        jl .loopstr
 
 
-    __movcur 05, 1
-    __print_b10_i8 [regdata.ax]
-    __movcur 14, 1
-    __print_b2_i8  [regdata.ax]
     __movcur 05, 2
-    __print_b10_i8 [regdata.ax + 1]
+    __print_b10_i8 [regdata.ax]
     __movcur 14, 2
-    __print_b2_i8  [regdata.ax + 1]
+    __print_b2_i8  [regdata.ax]
+    __movcur 34, 2
+    __print_b16_i8  [regdata.ax]
 
     __movcur 05, 3
-    __print_b10_i8 [regdata.bx]
+    __print_b10_i8 [regdata.ax + 1]
     __movcur 14, 3
-    __print_b2_i8  [regdata.bx]
+    __print_b2_i8  [regdata.ax + 1]
+    __movcur 34, 3
+    __print_b16_i8  [regdata.ax + 1]
+
     __movcur 05, 4
-    __print_b10_i8 [regdata.bx + 1]
+    __print_b10_i8 [regdata.bx]
     __movcur 14, 4
-    __print_b2_i8  [regdata.bx + 1]
+    __print_b2_i8  [regdata.bx]
+    __movcur 34, 4
+    __print_b16_i8  [regdata.bx]
 
     __movcur 05, 5
-    __print_b10_i8 [regdata.cx]
+    __print_b10_i8 [regdata.bx + 1]
     __movcur 14, 5
-    __print_b2_i8  [regdata.cx]
+    __print_b2_i8  [regdata.bx + 1]
+    __movcur 34, 5
+    __print_b16_i8  [regdata.bx + 1]
+
     __movcur 05, 6
-    __print_b10_i8 [regdata.cx + 1]
+    __print_b10_i8 [regdata.cx]
     __movcur 14, 6
-    __print_b2_i8  [regdata.cx + 1]
+    __print_b2_i8  [regdata.cx]
+    __movcur 34, 6
+    __print_b16_i8  [regdata.cx]
 
     __movcur 05, 7
-    __print_b10_i8 [regdata.dx]
+    __print_b10_i8 [regdata.cx + 1]
     __movcur 14, 7
-    __print_b2_i8  [regdata.dx]
+    __print_b2_i8  [regdata.cx + 1]
+    __movcur 34, 7
+    __print_b16_i8  [regdata.cx + 1]
+
     __movcur 05, 8
-    __print_b10_i8 [regdata.dx + 1]
+    __print_b10_i8 [regdata.dx]
     __movcur 14, 8
+    __print_b2_i8  [regdata.dx]
+    __movcur 34, 8
+    __print_b16_i8  [regdata.dx]
+
+    __movcur 05, 9
+    __print_b10_i8 [regdata.dx + 1]
+    __movcur 14, 9
     __print_b2_i8  [regdata.dx + 1]
+    __movcur 34, 9
+    __print_b16_i8  [regdata.dx + 1]
 
     __movcur 05, 10
     __print_b10_i16 [regdata.sp]
     __movcur 14, 10
     __print_b2_i16  [regdata.sp]
+    __movcur 34, 10
+    __print_b16_i16  [regdata.sp]
 
     __movcur 05, 11
     __print_b10_i16 [regdata.bp]
     __movcur 14, 11
     __print_b2_i16  [regdata.bp]
+    __movcur 34, 11
+    __print_b16_i16  [regdata.bp]
 
-    __movcur 0, 12
+    __movcur 0, 13
 
     ret
 .:
@@ -159,7 +171,7 @@ printdec:
     mov cx, 10
 
     .loop:    
-        cmp ax, 10
+        cmp ax, cx
         jl .print
 
         xor dx, dx
@@ -192,7 +204,7 @@ printbin:
     mov cx, 2
 
     .loop:    
-        cmp ax, 2
+        cmp ax, cx
         jl .print
 
         xor dx, dx
@@ -205,6 +217,46 @@ printbin:
 
     .print:
 
+        add ax, '0'
+
+        call printchar
+
+        dec bx
+        or bx, bx
+        jz .exit
+
+        pop ax
+        jmp .print
+
+    .exit:
+        ret
+.:
+
+printhex:
+    mov bx, 1
+    mov cx, 16
+
+    .loop:    
+        cmp ax, cx
+        jl .print
+
+        xor dx, dx
+
+        div cx
+
+        push dx
+        inc bx
+        jmp .loop
+
+
+    .print:
+
+        cmp ax, 9
+        jl .below10
+
+        add ax, 7
+
+    .below10:
         add ax, '0'
 
         call printchar
--- src/common/x16/print16.nasm
+++ src/common/x16/print16.nasm
@@ -3,9 +3,16 @@ BITS 16
 extern printcstr
 extern printdec
 extern printbin
+extern printhex
 extern printregs
+extern printchar
 extern clearscreen
 extern setcurpos
+
+
+%macro __printregs 0
+    call printregs
+%endmacro
 
 %macro __print_b10_i8 1 
     xor ax, ax
@@ -27,6 +34,17 @@ extern setcurpos
 %macro __print_b2_i16 1
     mov word ax, %1
     call printbin
+%endmacro
+
+%macro __print_b16_i8 1 
+    xor ax, ax
+    mov byte al, %1
+    call printhex
+%endmacro
+
+%macro __print_b16_i16 1
+    mov word ax, %1
+    call printhex
 %endmacro
 
 %macro __printcstr 1
