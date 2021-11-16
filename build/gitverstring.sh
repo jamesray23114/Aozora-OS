40c44066226bad65af1d690b6856512d49e52b86
jamesray23114 <jamesray23114@gmail.com>
2021-11-16 03:55:54 -0800

    Aozora-OS 1.1.2 -> cleaned up versioning files

--- build/gitverstring.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MAJOR=$(sed '1!d' build/VERSION)
-MINOR=$(sed '2!d' build/VERSION)
-PATCH=$(sed '3!d' build/VERSION)
-export VERSTRING="Aozora-OS-$MAJOR.$MINOR.$PATCH ->"
-PATCH=$(($PATCH+1))
-printf "$MAJOR\n$MINOR\n$PATCH" > build/VERSION
